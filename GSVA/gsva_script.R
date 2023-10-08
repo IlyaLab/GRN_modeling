264ec0c8e563b1fa2ff8ef79fcea3bca2e6dd582
Tazein-S <138258831+Tazein-S@users.noreply.github.com>
2023-10-07 22:15:39 -0400

    Updated 10/7

--- GSVA/gsva_script.R
+++ GSVA/gsva_script.R
@@ -1,9 +1,11 @@
 # load necessary packages 
 library(readxl)
 library(GSVA)
+library(ggplot2)
+library(dplyr)
 
 # importing the excel sheet (CPM)
-s8_original_df <- read_excel("C:/Users/15167/OneDrive/Documents/ISB/AML-DT-BNM/s8_table.xlsx")
+s8_original_df <- read_excel("C:/Users/15167/OneDrive/Documents/ISB/AML-DT-BNM/raw_data/s8_table.xlsx")
 s8_df <- s8_original_df[,!names(s8_original_df) %in% c("Gene", "Symbol")]
 
 #fix dataframe to convert to matrix
@@ -11,14 +13,29 @@ colnames(s8_df) <- paste0("s", 1:ncol(s8_df))
 rownames(s8_df) <- paste0("g", 1:nrow(s8_df))
 s8_matrix <- as.matrix(s8_df)
 
-#creating 100 sample gene sets 
-p = nrow(s8_df) #number of genes
-n = ncol(s8_df) #number of samples
-gs <- as.list(sample(10:100, size=100, replace=TRUE))
+#finding out the corresponding gene like (FLT3) to the matrix (g5)
+#which(s8_original_df$Symbol == "DPM1")
 
-gs <- lapply(gs, function(n, p)
-  paste0("g", sample(1:p, size=n, replace=FALSE)), p)
-names(gs) <- paste0("gs", 1:length(gs))
+#create three gene sets as character vectors (based off fig 3 equations)
+proliferation <- c("g5073","g10378","g6394","g5815","g14242","g7345","g3318",
+                   "g8201","g5899","g10484","g3110","g4875", "g4574", "g11852") 
+
+differentiation <- c("g871","g4373","g2048","g8811","g19209","g6715","g7803")
+
+apoptosis <- c("g7017","g430","g1538","g19","g3287", "g7994", "g4261", "g12786", "g14475") #used all the FOXO family 
+
+#combine the character vectors into a list
+gs <- list(
+  gs_prolif = proliferation,
+  gs_diff = differentiation,
+  gs_apop = apoptosis
+)
 
 #getting the GSVA scores mx.diff=TRUE, so calculates scores dependent of each other
-gsva_test <- gsva(s8_matrix, gs, mx.diff= TRUE, verbose = FALSE)
+gsva_test <- gsva(s8_matrix, gs, mx.diff = TRUE, verbose = TRUE)
+
+#tranposing the resukts and then turning it into a dataframe
+data <- (data.frame(t(gsva_test)))
+
+#calculating the network_score for each patient
+data$network_score <- data$gs_prolif - ((data$gs_diff) + (data$gs_apop))
